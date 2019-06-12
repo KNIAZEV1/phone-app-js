7f61c7b02c248f1ba80451de68448cfb862be597
ilia <ilia.kniazev1@gmail.com>
2019-06-12 16:40:18 +0300

    rendering optimization was added

--- js/Component.js
+++ js/Component.js
@@ -2,6 +2,7 @@ export default class Component {
   constructor(element, props = {}) {
     this.element = element;
     this.props = props;
+    this.children = {};
   }
 
   on(eventName, elementName, callback) {
@@ -25,6 +26,24 @@ export default class Component {
     };
 
     this.render();
+  }
+
+  initComponent(constructor, props) {
+    const container = this.element.querySelector(constructor.name);
+
+    if (!container) {
+      delete this.children[constructor.name];
+
+      return;
+    }
+
+    const current = this.children[constructor.name];
+
+    if (current && _.isEqual(props, current.props)) {
+      container.replaceWith(current.element);
+    } else {
+      this.children[constructor.name] = new constructor(container, props);
+    }
   }
 
 }
